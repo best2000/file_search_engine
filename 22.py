5680686ee775b5a1d55edde0184feeecb96714d5
best2000 <silentman@hotmail.co.th>
2020-02-22 17:29:28 +0700

    lol

---
+++ 22.py
@@ -0,0 +1,4 @@
+str = "lol"
+key = "lol"
+x = str.find(key, 1,len(str))
+print(x)
--- t.py
+++ t.py
@@ -8,7 +8,7 @@ def read_index(root_path):
     with open("file_index.pkl", 'wb') as f:
         pickle.dump(file_index, f)
 
-def search(key, search_type):
+def search(key, search_type, f_ex):
     #load file_index.pkl
     with open("file_index.pkl", 'rb') as f:
         file_index = pickle.load(f)
@@ -18,16 +18,32 @@ def search(key, search_type):
     match = 0
     result = []
     for path, files in file_index:
-        if (search_type == "contain" or search_type == None):
-                for f in files:
-                    all += 1
-                    if (key in f) == True:
-                        match += 1
-                        result.append(path+"/"+f)
-        elif (search_type == "startwith"):
-            pass
-        elif (search_type == "endwith"):
-            pass
+        for f in files:
+            all += 1
+            #remove file extension from name
+            if f_ex == False:
+                f2 = os.path.splitext(f)[0]
+            else:
+                f2 = f
+            #search by search_type
+            if key == f2: #it the same
+                    match += 1
+                    result.append(path+"/"+f)
+            elif (search_type == "contain" or search_type == None): #contain keyword  
+                if (key in f2) == True:
+                    match += 1
+                    result.append(path+"/"+f)
+            elif (search_type == "startwith"): #start with keyword
+                if f2.find(key) == 0: 
+                    match += 1
+                    result.append(path+"/"+f)
+            elif (search_type == "endwith"): #end with keyword
+                f_len = len(f2)
+                key_len = len(key)
+                key_start_index = f2.find(key, f_len-key_len-1, f_len)
+                if (key_start_index != -1): 
+                    match += 1
+                    result.append(path+"/"+f)
     
     print(">> found", match, "out of", all, "files")
     for i in result:
@@ -35,4 +51,4 @@ def search(key, search_type):
 
     
 #read_index("C:/CODE/file_search_engine")
-search("t", "contain")
+search("ol", "endwith", False)
